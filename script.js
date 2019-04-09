79126d2cd4d1c62f483da5dc0b2748c97f82500e
foreverurs <49413992+foreverurs@users.noreply.github.com>
2019-04-09 23:29:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const message =`Ek Pal Ki Ye Baat Nahi +
+const message1 =`Ek Pal Ki Ye Baat Nahi +
 				 Do Pal Ka Ye Sath Nahi +
 				 Kehne Ko Toh Ye Zindagi..Jannat Se Pyaari hai +
 				 Par Vo Sath Hi Kya Jisme Apka Hath Nahi`
-				 
+const message2 =`Dil Ke Kone Se Ek Awaaz Aati Hai+
+				 Hame Har Pal Unki Yaad Aati Hai+
+				 Dil Puchta Hai Bar Bar Hamse+
+				 Ke Jitna Hum Yaad Karte Hai Unhe+
+				 Kya Unhe Bhi Hmari Yaad Aati Hai`
+let data=[];
+data.push(message2);
+data.push(message1);
 let login=document.getElementById('login');
 let messagediv=document.getElementById('messagediv');
 
@@ -13,11 +20,16 @@ function submit(){
 	if((document.getElementById('password').value).toLowerCase()=='dilse'||(document.getElementById('password').value).toLowerCase()=='dil se'){
 		login.style.display='none';
 		messagediv.style.display='block';
-		let array=message.split('+');
-		for(a of array){
-			let div=document.createElement('div');
-			div.innerHTML=a.trim();
-			messagediv.appendChild(div);
+
+		for(d of data){
+			let array=d.split('+');
+			let div;
+			for(a of array){
+				div=document.createElement('div');
+				div.innerHTML=a.trim();
+				messagediv.appendChild(div);
+			}
+			div.style.paddingBottom='50px';
 		}
 	}
 }
